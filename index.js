24ca749a5e3674f78f6d169f57fd77aefad8bd37
bryandms <bryandms95@gmail.com>
2020-08-04 12:44:15 -0600

    Setup

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = createApp();
 mongoose
   .connect(DB_URI, DB_OPTIONS)
   .then(() =>
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+    app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server listening on port ${PORT}`)
+    )
   )
   .catch((err) => console.log(err));
